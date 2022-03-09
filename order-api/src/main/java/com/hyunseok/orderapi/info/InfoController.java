e13df6c45fb9eba9c7aaab5d651b59dc00921085
전현석 <hsjeon@catchsecu.com>
2022-03-09 21:57:54 +0900

    요청을 api gateway -> 개별 서비스로 요청

--- order-api/src/main/java/com/hyunseok/orderapi/info/InfoController.java
+++ order-api/src/main/java/com/hyunseok/orderapi/info/InfoController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfoController {
 
-    @GetMapping("/info")
-    public String info(@Value("${}") String port) {
+    @GetMapping("/order/info")
+    public String info(@Value("${server.port}") String port) {
         return "Order 서비스 기본 동작 Port : " + port;
     }
 }
