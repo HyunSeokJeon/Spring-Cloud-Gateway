39c56991a9997c4413a6a86c506365662039b290
전현석 <hsjeon@catchsecu.com>
2022-03-09 21:26:40 +0900

    test api 생성

---
+++ user-api/src/main/java/com/hyunseok/userapi/info/InfoController.java
@@ -0,0 +1,14 @@
+package com.hyunseok.userapi.info;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @GetMapping("/info")
+    public String info(@Value("${}") String port) {
+        return "User 서비스 기본 동작 Port : " + port;
+    }
+}
